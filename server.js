b88ba64f44068b58ac9e19ce0930d642afcbca51
SuvorkovRN <suvorkovRN@gmail.com>
2020-05-02 11:10:20 +0500

    CORS copy deleted

--- server.js
+++ server.js
@@ -52,32 +52,32 @@ app.use(koaBody({
 
 let imgContainer = [];
 
-app.use(async (ctx, next) => {
+app.use(async ctx => {
 
-  const origin = ctx.request.get('Origin');
-    if (!origin) {
-        return await next();
-    }
-    const headers = { 'Access-Control-Allow-Origin': '*', };
-    if (ctx.request.method !== 'OPTIONS') {
-        ctx.response.set({...headers});
-        try {
-            return await next();
-        } catch (e) {
-            e.headers = {...e.headers, ...headers};
-            throw e;
-        }
-    }
-    if (ctx.request.get('Access-Control-Request-Method')) {
-        ctx.response.set({
-            ...headers,
-            'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH',
-        });
-        if (ctx.request.get('Access-Control-Request-Headers')) {
-            ctx.response.set('Access-Control-Allow-Headers', ctx.request.get('Access-Control-Allow-Request-Headers'));
-        }
-        ctx.response.status = 204; // No content
-    }
+  // const origin = ctx.request.get('Origin');
+  //   if (!origin) {
+  //       return await next();
+  //   }
+  //   const headers = { 'Access-Control-Allow-Origin': '*', };
+  //   if (ctx.request.method !== 'OPTIONS') {
+  //       ctx.response.set({...headers});
+  //       try {
+  //           return await next();
+  //       } catch (e) {
+  //           e.headers = {...e.headers, ...headers};
+  //           throw e;
+  //       }
+  //   }
+  //   if (ctx.request.get('Access-Control-Request-Method')) {
+  //       ctx.response.set({
+  //           ...headers,
+  //           'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH',
+  //       });
+  //       if (ctx.request.get('Access-Control-Request-Headers')) {
+  //           ctx.response.set('Access-Control-Allow-Headers', ctx.request.get('Access-Control-Allow-Request-Headers'));
+  //       }
+  //       ctx.response.status = 204; // No content
+  //   }
 
 
 
